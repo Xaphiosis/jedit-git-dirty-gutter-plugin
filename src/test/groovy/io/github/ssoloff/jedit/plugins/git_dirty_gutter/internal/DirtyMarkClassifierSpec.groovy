5f38e60af5a2c414de0ee4edb133ee8505308989
Steven Soloff <s_soloff@yahoo.com>
2015-12-21 17:53:01 -0500

    Add unit test for classifying in the presence of mixed diff hunks.

--- src/test/groovy/io/github/ssoloff/jedit/plugins/git_dirty_gutter/internal/DirtyMarkClassifierSpec.groovy
+++ src/test/groovy/io/github/ssoloff/jedit/plugins/git_dirty_gutter/internal/DirtyMarkClassifierSpec.groovy
@@ -102,7 +102,7 @@ class DirtyMarkClassifierSpec extends Specification {
         def patch = new Patch()
         patch.addDelta(new ChangeDelta(
             new Chunk(0, ['1']),
-            new Chunk(0, [])
+            new Chunk(0, []) // NB: difflib behavior: 0(1) -> -1(0) -> 0(0)
         ))
         patch.addDelta(new ChangeDelta(
             new Chunk(9, ['10']),
@@ -122,5 +122,110 @@ class DirtyMarkClassifierSpec extends Specification {
         classifier.classifyLine(8) == DirtyMarkType.UNCHANGED
         classifier.classifyLine(15) == DirtyMarkType.UNCHANGED
         classifier.classifyLine(16) == DirtyMarkType.REMOVED_BELOW
+    }
+
+    def 'classifyLine should classify a mixed collection of added, changed, and removed lines'() {
+        // --- from.mixed	2015-12-21 17:33:49.971052965 -0500
+        // +++ to.mixed	2015-12-21 17:34:43.429408252 -0500
+        // @@ -0,0 +1,6 @@
+        // +This is an important
+        // +notice! It should
+        // +therefore be located at
+        // +the beginning of this
+        // +document!
+        // +
+        // @@ -8,7 +14 @@
+        // -compress the size of the
+        // -changes.
+        // -
+        // -This paragraph contains
+        // -text that is outdated.
+        // -It will be deleted in the
+        // -near future.
+        // +compress anything.
+        // @@ -17 +17 @@
+        // -check this dokument. On
+        // +check this document. On
+        // @@ -24,0 +25,4 @@
+        // +
+        // +This paragraph contains
+        // +important new additions
+        // +to this document.
+
+        setup:
+        def patch = new Patch()
+        patch.addDelta(new ChangeDelta(
+            new Chunk(0, []), // NB: difflib behavior: 0(1) -> -1(0) -> 0(0)
+            new Chunk(0, [
+                'This is an important',
+                'notice! It should',
+                'therefore be located at',
+                'the beginning of this',
+                'document!',
+                ''
+            ])
+        ))
+        patch.addDelta(new ChangeDelta(
+            new Chunk(7, [
+                'compress the size of the',
+                'changes.',
+                '',
+                'This paragraph contains',
+                'text that is outdated.',
+                'It will be deleted in the',
+                'near future.'
+            ]),
+            new Chunk(13, [
+                'compress anything.'
+            ])
+        ))
+        patch.addDelta(new ChangeDelta(
+            new Chunk(16, [
+                'check this dokument. On'
+            ]),
+            new Chunk(16, [
+                'check this document. On'
+            ])
+        ))
+        patch.addDelta(new ChangeDelta(
+            new Chunk(23, []),
+            new Chunk(24, [
+                '',
+                'This paragraph contains',
+                'important new additions',
+                'to this document.'
+            ])
+        ))
+        def classifier = new DirtyMarkClassifier(patch)
+
+        expect:
+        classifier.classifyLine(0) == DirtyMarkType.ADDED
+        classifier.classifyLine(1) == DirtyMarkType.ADDED
+        classifier.classifyLine(2) == DirtyMarkType.ADDED
+        classifier.classifyLine(3) == DirtyMarkType.ADDED
+        classifier.classifyLine(4) == DirtyMarkType.ADDED
+        classifier.classifyLine(5) == DirtyMarkType.ADDED
+        classifier.classifyLine(6) == DirtyMarkType.UNCHANGED
+        classifier.classifyLine(7) == DirtyMarkType.UNCHANGED
+        classifier.classifyLine(8) == DirtyMarkType.UNCHANGED
+        classifier.classifyLine(9) == DirtyMarkType.UNCHANGED
+        classifier.classifyLine(10) == DirtyMarkType.UNCHANGED
+        classifier.classifyLine(11) == DirtyMarkType.UNCHANGED
+        classifier.classifyLine(12) == DirtyMarkType.UNCHANGED
+        classifier.classifyLine(13) == DirtyMarkType.CHANGED
+        classifier.classifyLine(14) == DirtyMarkType.UNCHANGED
+        classifier.classifyLine(15) == DirtyMarkType.UNCHANGED
+        classifier.classifyLine(16) == DirtyMarkType.CHANGED
+        classifier.classifyLine(17) == DirtyMarkType.UNCHANGED
+        classifier.classifyLine(18) == DirtyMarkType.UNCHANGED
+        classifier.classifyLine(19) == DirtyMarkType.UNCHANGED
+        classifier.classifyLine(20) == DirtyMarkType.UNCHANGED
+        classifier.classifyLine(21) == DirtyMarkType.UNCHANGED
+        classifier.classifyLine(22) == DirtyMarkType.UNCHANGED
+        classifier.classifyLine(23) == DirtyMarkType.UNCHANGED
+        classifier.classifyLine(24) == DirtyMarkType.ADDED
+        classifier.classifyLine(25) == DirtyMarkType.ADDED
+        classifier.classifyLine(26) == DirtyMarkType.ADDED
+        classifier.classifyLine(27) == DirtyMarkType.ADDED
     }
 }
