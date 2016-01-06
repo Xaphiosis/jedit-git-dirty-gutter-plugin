685a1205af530ddf506c45eac6d0188da485e961
Steven Soloff <s_soloff@yahoo.com>
2016-01-05 20:47:35 -0500

    Suppress LineLength rule violation

--- src/main/java/io/github/ssoloff/jedit/plugins/git_dirty_gutter/internal/ui/GitBufferHandlerContext.java
+++ src/main/java/io/github/ssoloff/jedit/plugins/git_dirty_gutter/internal/ui/GitBufferHandlerContext.java
@@ -35,7 +35,9 @@ import org.gjt.sp.util.Log;
  * bridge to the jEdit API.
  */
 final class GitBufferHandlerContext implements IGitBufferHandlerContext {
+    //CHECKSTYLEOFF:LineLength
     private static final IDirtyMarkPainterSpecificationFactoryContext DIRTY_MARK_PAINTER_SPECIFICATION_FACTORY_CONTEXT = createDirtyMarkPainterSpecificationFactoryContext();
+    //CHECKSTYLEON:LineLength
     private static final ISupplier<Path> GIT_PROGRAM_PATH_SUPPLIER = createGitProgramPathSupplier();
     private static final ILog LOG = createLog();
 
